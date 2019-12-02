1a2c520b3829885442d10ceb594b95e802dbc82d
thyyt <tatu.hyytiainen@aalto.fi>
2019-12-02 20:26:16 +0200

    Adds start of second part

--- 2/computer.py
+++ 2/computer.py
@@ -1,7 +1,3 @@
-import readline
-import numpy as np
-
-
 def read_inputs(input_file="input"):
     with open(input_file) as f:
         line = f.readline()
@@ -12,9 +8,19 @@ def read_inputs(input_file="input"):
 
 class Computer:
     def __init__(self, inputs):
+        self.inputs = inputs
         self.memory = inputs
         self.position = 0
         self.halted = False
+
+    def reset(self):
+        self.halted = False
+        self.position = 0
+        self.memory = self.inputs
+
+    def insert_noun_and_verb(self, noun, verb):
+        self.memory[1] = noun
+        self.memory[2] = verb
 
     def initial_replacements(self, mappings):
         for key, value in mappings.items():
@@ -35,7 +41,6 @@ class Computer:
             left_input = self.memory[self.memory[self.position + 1]]
             right_input = self.memory[self.memory[self.position + 2]]
             output_position = self.memory[self.position + 3]
-            print(left_input, right_input, opcode, output_position)
             self.memory[output_position] = self.operate(left_input, right_input, opcode)
             self.position += 4
 
@@ -43,9 +48,7 @@ class Computer:
         while not self.halted:
             self.next_operation()
 
-        print("State of the memory after halt:")
-        print(self.memory)
-        print(f"Position 0 after halt: {self.memory[0]}")
+        return self.memory[0]
 
     def get_memory(self):
         return self.memory
@@ -55,7 +58,31 @@ def compute_first_part():
     inputs = read_inputs()
     first_computer = Computer(inputs)
     first_computer.initial_replacements({1: 12, 2: 2})
-    first_computer.compute()
+    output = first_computer.compute()
+    print("State of the memory after halt:")
+    print(first_computer.get_memory())
+    print(f"Position 0 after halt: {output}")
+
+
+def find_noun_and_verb(output=19690720):
+    inputs = read_inputs()
+    computer = Computer(inputs)
+    for noun in range(100):
+        for verb in range(100):
+            computer.reset()
+            old_memory = computer.get_memory()
+            computer.insert_noun_and_verb(noun, verb)
+            new_memory = computer.get_memory()
+
+            print(sum([a - b for a, b in zip(old_memory, new_memory)]))
+            # print(computer.get_memory())
+            program_output = computer.compute()
+
+            print(noun, verb, program_output)
+
+            if program_output == output:
+                print("Noun and verb to produce {output} are: {noun}, {verb}")
+                return noun, verb
 
 
 test_computer1 = Computer([1, 0, 0, 0, 99])
@@ -76,3 +103,5 @@ assert test_computer4.get_memory() == [30, 1, 1, 4, 2, 5, 6, 0, 99]
 
 if __name__ == "__main__":
     compute_first_part()
+
+    find_noun_and_verb()
