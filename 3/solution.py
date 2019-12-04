8e01393ae8cfedaff38bafbd1553c7844aab75d2
thyyt <tatu.hyytiainen@aalto.fi>
2019-12-04 20:39:29 +0200

    Progress

--- 3/solution.py
+++ 3/solution.py
@@ -15,29 +15,70 @@ def read_inputs(input_file="input"):
     return wire_instructions
 
 
-def compute_wire_dimensions(wire_instructions, dimension="height"):
-    position = 0
-    positions = [position]
-    dimension_instructions = [
-        instruction
-        for instruction in wire_instructions
-        if instruction[0] in LETTERS[dimension]
-    ]
-    for instruction in dimension_instructions:
-        if instruction[0] == NEGATIVE_DIRECTIONS[dimension]:
-            position -= int(instruction[1:])
-        else:
-            position += int(instruction[1:])
-        positions.append(position)
-
-    return min(positions), max(positions)
-
-
 class WireGrid:
-    def __init__(self, width, height, origin):
-        self.origin = origin
-        self.grid = np.ndarray((height, width))
+    def __init__(self, wire_instructions):
+        width_dimensions = [
+            self.compute_wire_dimensions(instruction, "width")
+            for instruction in wire_instructions
+        ]
+        height_dimensions = [
+            self.compute_wire_dimensions(instruction, "height")
+            for instruction in wire_instructions
+        ]
+        self.width, origin_w = self.compute_dimension_range(width_dimensions)
+        self.height, origin_h = self.compute_dimension_range(height_dimensions)
+        self.origin = (origin_h, origin_w)
+        self.wires = []
+
+        for instructions in wire_instructions:
+            wire = self.create_wire(instructions)
+            self.wires.append(wire)
+
+    def compute_wire_dimensions(self, wire_instructions, dimension="height"):
+        position = 0
+        positions = [position]
+        dimension_instructions = [
+            instruction
+            for instruction in wire_instructions
+            if instruction[0] in LETTERS[dimension]
+        ]
+        for instruction in dimension_instructions:
+            if instruction[0] == NEGATIVE_DIRECTIONS[dimension]:
+                position -= int(instruction[1:])
+            else:
+                position += int(instruction[1:])
+            positions.append(position)
+
+        return min(positions), max(positions)
+
+    def compute_dimension_range(self, dimensions):
+        low = min([dim[0] for dim in dimensions])
+        high = max([dim[1] for dim in dimensions])
+
+        size = abs(high) + abs(low)
+
+        return size, -low
+
+    def create_wire(self, instructions):
+        position_h = self.origin[0]
+        position_w = self.origin[1]
+        wire_location = np.zeros((self.height, self.width))
+
+        for instruction in instructions:
+            direction = instruction[0]
+            length = int(instruction[1:])
+            if direction == "D":
+                wire_location[position_h : position_h + length, position_w] = np.ones(
+                    length
+                )
+                position_h += length
+            elif direction == "U":
+                wire_location[position_h - length : position_h, position_w] = np.ones(
+                    length
+                )
+                position_h -= length
 
 
 if __name__ == "__main__":
-    read_inputs()
+    instructions = read_inputs()
+    first_grid = WireGrid(instructions)
